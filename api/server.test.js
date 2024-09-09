224b1bae34a7f6c2ed2720b15a0d30de1382af46
Jacob Handley <skaarjshark@outlook.com>
2024-09-09 15:13:56 -0500

    new commit. struggling on making the automated tests, but getting it to work.

--- api/server.test.js
+++ api/server.test.js
@@ -1,23 +1,43 @@
 const request = require('supertest')
+const express = require('express')
 const server = require('./server')
 const db = require('../data/dbConfig')
+const app = require('./server')
 
 beforeAll(async () => {
   await db.migrate.rollback(); // so any changes to migration files are picked up
   await db.migrate.latest();
 })
 
+afterAll(async () => {
+  await db.destroy(); // Close the database connection
+});
+
 test('sanity', () => {
   expect(true).toBe(true)
 })
 describe('[POST] /register', () => {
-  it('rejects new users with the same username as an existing one', () => {
+  it('rejects new users with the same username as an existing one', async () => {
+    await request(app)
+      .post('/api/auth/register')
+      .send({ username: 'Captain Marvel', password: 'foobar' });
 
+    // Test registration with the same username
+    const response = await request(app)
+      .post('/api/auth/register')
+      .send({ username: 'Captain Marvel', password: 'newpassword' });
+
+    expect(response.body.message).toBe('username taken');
   })
   it('returns the new user', async () => {
-    // const res = await request(server).post('/register').send({ username: 'Superman', password: 'foobar' })
-    // console.log(res)
-    // expect(res.body.username).toBe('Superman')
+    const response = await request(app)
+      .post('/api/auth/register')
+      .send({ username: 'Superman', password: 'foobar' })
+
+    const user = response.body[0]
+    expect(response.status).toBe(200)
+    expect(user.username).toBe('Superman')
+    expect(user.password).not.toBe('foobar')
   })
 })
 
